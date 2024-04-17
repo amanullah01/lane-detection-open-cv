5f38b74075b9d2cc54ca45718df2090d90c7533f
Amanullah Aman <amancse.aiub@gmail.com>
2024-04-17 12:28:51 +0600

    videos frame convert to grayscale and edge detect using cannys algorithm

--- LaneDetection.py
+++ LaneDetection.py
@@ -1,9 +1,22 @@
 import numpy as np
 import cv2
 
-# video = several frame of pictures
 
+def get_detected_lanes(image):
+    (height, width) = (image.shape[0], image.shape[1])
+
+    # convert color image to gray scale image
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # edge detection kernel (Canny s algorithm)
+    canny_image = cv2.Canny(gray_image, 100, 200)
+
+    return canny_image
+
+
+# video = several frame of pictures
 video = cv2.VideoCapture('videos/lane_detection_video.mp4')
+# video = cv2.VideoCapture('videos/v2.mp4')
 
 while video.isOpened():
 
@@ -14,6 +27,8 @@ while video.isOpened():
 
     if not is_grabbed:
         break
+
+    frame = get_detected_lanes(frame)
 
     cv2.imshow("Lane detection video", frame)
     cv2.waitKey(20)
