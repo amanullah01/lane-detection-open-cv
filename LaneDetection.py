2f32e44ee76be68a8e84079b49fc683955f9e663
Amanullah Aman <amancse.aiub@gmail.com>
2024-04-17 13:40:32 +0600

    keep only our interest area, others make blak

--- LaneDetection.py
+++ LaneDetection.py
@@ -2,6 +2,19 @@ import numpy as np
 import cv2
 
 
+def region_of_interest(image, region_points):
+
+    # We are replacing pixels with 0 (black) - the region we are not interested
+    mask = np.zeros_like(image)
+
+    # the region we are interested in the lower triangle - replace with 255
+    cv2.fillPoly(mask, region_points, 255)
+
+    masked_image = cv2.bitwise_and(image, mask)
+    return masked_image
+
+
+
 def get_detected_lanes(image):
     (height, width) = (image.shape[0], image.shape[1])
 
@@ -11,12 +24,22 @@ def get_detected_lanes(image):
     # edge detection kernel (Canny s algorithm)
     canny_image = cv2.Canny(gray_image, 100, 200)
 
-    return canny_image
+    # we are interested about the lower region of the image (There are the driving lanes)
+    region_of_interested_vertices = [
+        (0, height),
+        (width/2, height*0.65),
+        (width, height)
+    ]
+
+    # we can get rid of the un-relevant part of the image
+    # We just keep the lower triangle region
+    cropped_image = region_of_interest(canny_image, np.array([region_of_interested_vertices], np.int32))
+
+    return cropped_image
 
 
 # video = several frame of pictures
 video = cv2.VideoCapture('videos/lane_detection_video.mp4')
-# video = cv2.VideoCapture('videos/v2.mp4')
 
 while video.isOpened():
 
