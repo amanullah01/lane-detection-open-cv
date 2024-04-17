a3b41721ff82d3d6f3f68296c1fcdf31666b93e8
Amanullah Aman <amancse.aiub@gmail.com>
2024-04-17 14:21:45 +0600

    draw the lines

--- LaneDetection.py
+++ LaneDetection.py
@@ -2,6 +2,20 @@ import numpy as np
 import cv2
 
 
+def draw_lines(image, lines):
+    # create a distinct image for the lines [0,255] - all the 0 values are black
+    lines_image = np.zeros((image.shape[0], image.shape[1], 3), dtype=np.uint8)
+
+    # there are x, y points for starting and end points for linex
+    for line in lines:
+        for x1, y1, x2, y2 in line:
+            cv2.line(image, (x1, y1), (x2, y2), (0, 255, 0), thickness=3)
+
+    # finally we have to merge the  image  with lines
+    image_with_lines = cv2.addWeighted(image, 0.8, lines_image, 1, 0.0)
+    return image_with_lines
+
+
 def region_of_interest(image, region_points):
 
     # We are replacing pixels with 0 (black) - the region we are not interested
@@ -35,7 +49,13 @@ def get_detected_lanes(image):
     # We just keep the lower triangle region
     cropped_image = region_of_interest(canny_image, np.array([region_of_interested_vertices], np.int32))
 
-    return cropped_image
+    # use line detection algorithm. (radians instead of degrees. 1 degree = pi/180)
+    lines = cv2.HoughLinesP(cropped_image, rho=2, theta=np.pi/180, threshold=50, lines=np.array([]), minLineLength=40, maxLineGap=150)
+
+    # draw the lines with image
+    image_with_lines = draw_lines(image, lines)
+
+    return image_with_lines
 
 
 # video = several frame of pictures
